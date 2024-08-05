07c61476a655c18b355f07f3256e716fdfb07b6e
Nathaniel-kw Cheung <nathanielkwcheung@gmail.coms>
2024-08-06 07:49:36 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geek_python_util',
+    version='1.0.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    description='A reusable geek util for Python applications',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='GeekGeekGo',
+    author_email='geekgeekgo@gmail.com',
+    url='https://github.com/geekgeekgo-io/geek_python_util',
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
---
+++ src/python_logger_util/logger_util.py
@@ -0,0 +1,59 @@
+import logging
+import json
+from datetime import datetime
+
+class CustomJsonFormatter(logging.Formatter):
+    def __init__(self, service_name):
+        super().__init__()
+        self.service_name = service_name
+
+    def format(self, record):
+        log_object = {
+            "timestamp": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "service": self.service_name,
+        }
+
+        # Add custom fields
+        log_object["custom_field"] = getattr(record, 'custom_field', 'default_custom_value')
+
+        # Include exception info if present
+        if record.exc_info:
+            log_object["exception"] = self.formatException(record.exc_info)
+
+        # Include any extra attributes from the record
+        for key, value in record.__dict__.items():
+            if key not in ["args", "asctime", "created", "exc_info", "exc_text", "filename",
+                           "funcName", "levelname", "levelno", "lineno", "module",
+                           "msecs", "message", "msg", "name", "pathname", "process",
+                           "processName", "relativeCreated", "thread", "threadName"]:
+                log_object[key] = value
+
+        return json.dumps(log_object)
+
+class LoggerUtil:
+    @staticmethod
+    def setup_logger(name, service_name, level=logging.INFO, custom_field=None):
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(CustomJsonFormatter(service_name=service_name))
+        logger.addHandler(console_handler)
+
+        # File handler
+        file_handler = logging.FileHandler(f"{service_name}.log")
+        file_handler.setFormatter(CustomJsonFormatter(service_name=service_name))
+        logger.addHandler(file_handler)
+
+        # Add custom field as a filter
+        class CustomFieldFilter(logging.Filter):
+            def filter(self, record):
+                record.custom_field = custom_field
+                return True
+
+        logger.addFilter(CustomFieldFilter())
+
+        return logger
