f2b563ddfbd4bd2caad28281816aa7d0cc76003d
Nathaniel-kw Cheung <nathanielkwcheung@gmail.coms>
2024-09-07 07:28:59 +0800

    fix syntax

--- trello_test.py
+++ trello_test.py
@@ -1,11 +1,12 @@
 from src.geek_trello_util import TrelloUtil
+
 # Replace these with your actual Trello API key, token, and list ID
-API_KEY =
-TOKEN =
-LIST_ID =
+API_KEY = ""
+TOKEN = ""
+LIST_ID = ""
 
 # FINDING LIST_ID (add .json to the end of trello link to find the board ID, and then inside find the name of the list name and then find the "id:" again
-#https://docs.pixiebrix.com/integrations/trello/find-board-and-list-ids-in-trello
+# https://docs.pixiebrix.com/integrations/trello/find-board-and-list-ids-in-trello
 # Example usage
 card_name = "New Task"
 card_description = "This is a new task added via the API"
